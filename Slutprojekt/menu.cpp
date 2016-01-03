d8b3166522ba1a32b9bfcbc0986fc9c0023326c1
rikardw <rikard.wiklander@gmail.com>
2016-01-03 12:09:42 +0100

    remade menu

--- Slutprojekt/menu.cpp
+++ Slutprojekt/menu.cpp
@@ -24,55 +24,68 @@ void selectionMenu(vector<person> personVector) {
 		{
 		case 1:
 			system("CLS");
+			cout << "***ADD PERSON***" << endl;
+			cin.ignore(1000, '\n'); //"flushar" funktionen så CLS fungerar i det här fallet, skapade problem annars
+			personVector = addPersonToList(personVector);
+			break;
+
+		case 2:
+			system("CLS");
 			cout << "***SEE LIST***" << endl;
 			printList(personVector);
 			system("pause");
 			break;
 
-		case 2:
-			system("CLS");
-			cout << "***ADD PERSON***" << endl;
-			cin.ignore(1000, '\n'); //"flushar" funktionen så CLS fungerar i det här fallet, skapade problem annars
-			personVector = addPersonToList(personVector);
-			break;
 		case 3:
-			system("CLS");
-			cout << "***REMOVE PERSON***" << endl;
-			personVector = removePerson(personVector);
-			system("pause");
-			
-			break;
-
-		case 4:
-			cout << "Randomize list" << endl;
-			// randomizeList(personVector)
-			break;
-
-		case 5:
-			cout << "Sort list" << endl;/*
-			personVector = sortList(personVector);*/
-			break;
-
-		case 6:
 			system("CLS");
 			cout << "***SEARCH***" << endl;
 			searchSignature(personVector);
 			system("pause");
 			break;
 
+		case 4:
+			system("CLS");
+			cout << "***REMOVE PERSON***" << endl;
+			personVector = removePerson(personVector);
+			system("pause");
+			break;
+
+		case 5:
+			cout << "***SORT LIST BY NAME***" << endl;/*
+			personVector = sortList(personVector);*/
+			break;
+
+		case 6:
+			cout << "***SORT LIST BY SIGNATURE***" << endl;/*
+			personVector = sortList(personVector);*/
+			break;
+
 		case 7:
+			cout << "***SORT LIST BY HEIGHT***" << endl;/*
+			personVector = sortList(personVector);*/
+			break;
+		
+		case 8:
+			system("CLS");
+			cout << "***RANDOMIZE LIST***" << endl;
+			//saveToFile(personVector);
+			system("pause");
+			break;
+
+		case 9:
+			system("CLS");
+			cout << "***SAVE LIST***" << endl;
+			saveToFile(personVector);
+			system("pause");
+			break;
+
+		case 10:
 			system("CLS");
 			cout << "***OPEN LIST***" << endl;
 			personVector = readFromFile(personVector);
 			system("pause");
 		break;
 
-		case 8:
-			system("CLS");
-			cout << "***SAVE LIST***" << endl;
-			saveToFile(personVector);
-			system("pause");
-			break;
 			
 		case 0:
 			cout << "gbye";
@@ -88,16 +101,17 @@ void selectionMenu(vector<person> personVector) {
 void menuItems(){
 	cout << endl;
 	cout << " ====================================" << endl;
-	cout << " 1. See list " << endl;
-	cout << " 2. Add person" << endl;
-	cout << " 3. Remove person" << endl;
-	cout << " 4. Randomize list" << endl;
-	cout << " 5. Sort list" << endl;
-	cout << " 6. Search" << endl;
-	cout << " 7. Open list" << endl;
-	cout << " 8. Save list" << endl;
-	cout << endl;
-	cout << " 0. Exit " << endl;
+	cout << " 1.  Add person " << endl;
+	cout << " 2.  Show list" << endl;
+	cout << " 3.  Search list" << endl;
+	cout << " 4.  Remove person" << endl;
+	cout << " 5.  Sort by name" << endl;
+	cout << " 6.  Sort by signature" << endl;
+	cout << " 7.  Sort by height" << endl;
+	cout << " 8.  Randomize list" << endl;
+	cout << " 9.  Save list to file" << endl;
+	cout << " 10. Read list from file" << endl;
+	cout << " 0.  Exit " << endl;
 	cout << " ====================================" << endl;
 	cout << " Enter your selection: ";
 
